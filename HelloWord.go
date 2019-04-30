f42df106f23cdcbcc51673cac962572bff7beec9
Landon Lemieux <landonlemieux@u.boisestate.edu>
2019-04-30 09:16:46 -0600

    Update HelloWord.go

--- HelloWord.go
+++ HelloWord.go
@@ -8,11 +8,11 @@ import (
 	"strings"
 )
 
-//HelloWord.go is a implementation of the golang based off of a finite automata.
-//An input file is provided as an argument and a string is taken from the user during
-//execution. The string is then broken to characters which are compared to the input
-//file. If the character matches, the string moves to the next character, else it
-//continues to move forward until a match is found or end of file is reached.
+/*HelloWord.go is a implementation of the golang based off of a finite automata.
+An input file is provided as an argument and a string is taken from the user during
+execution. The string is then broken to characters which are compared to the input
+file. If the character matches, the string moves to the next character, else it
+continues to move forward until a match is found or end of file is reached.*/
 type Word struct {
 	word   []byte
 	index  int
